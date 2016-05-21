41fec0c8a8f458e74e7f8f7e8bd2c0c70c32e9a1
miletskiy <s.miletskiy@gmail.com>
2016-05-22 02:23:55 +0300

    Fix calculation for total sum. Fix lender template

--- js/percent_calc.js
+++ js/percent_calc.js
@@ -4,19 +4,22 @@ $(document).ready(function() {
       period = '',
       period_days = 0,
       percent = 0.001,
-      your_get_span = $('#your-get'),
-      your_return_span = $('#your-return');
+      //variable for regulation payments for transaction
+      commissionOfService = 0.0042,
+      your_get_span = $('#you-get'),
+      your_return_span = $('#you-return');
 
   // Look for changes in the value
    how_much_input.bind("propertychange change click keyup input paste", function(event){
       // If value has changed...
       how_much = parseInt(how_much_input.val());
-  
+
       if (how_much !== 0 && period_days !== 0) {
 
-        your_get = parseFloat((percent * period_days * how_much).toFixed(2))
-        your_get_span.text(your_get)
-        your_return_span.text(your_get + how_much)
+        your_get = parseFloat((how_much).toFixed(2));
+        your_get_span.text(your_get);
+        var total_return = parseFloat((percent * period_days * how_much).toFixed(2));
+        your_return_span.text(total_return);
       };
 
       if (!how_much) {
@@ -29,25 +32,31 @@ $(document).ready(function() {
       }
    });
 
+  // helper function
+    function getPeriod(period) {
+        if (period == "day") {
+          period_days = 1;
+        } else if (period == "week") {
+          period_days = 7;
+        } else if (period == "1month") {
+          period_days = 30;
+        } else if (period == "6month") {
+          period_days = 30*6;
+        } else if (period == "year") {
+          period_days = 365;
+        };
+    }
+
    $('input:radio').change(
       function(){
           period = $(this).attr('id');
-          if (period == "day") {
-            period_days = 1;
-          } else if (period == "week") {
-            period_days = 7;
-          } else if (period == "1month") {
-            period_days = 30;
-          } else if (period == "6month") {
-            period_days = 30*6;
-          } else if (period == "year") {
-            period_days = 365;
-          };
+          getPeriod(period);
 
           if (how_much !== 0 && period_days !== 0) {
-            your_get = parseFloat((percent * period_days * how_much).toFixed(2))
-            your_get_span.text(your_get)
-            your_return_span.text(your_get + how_much)
+            your_get = parseFloat((how_much).toFixed(2));
+            your_get_span.text(your_get);
+            var total_return = parseFloat((percent * period_days * how_much + your_get).toFixed(2));
+            your_return_span.text(total_return);
           };
 
           if (!how_much) {
